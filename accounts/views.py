8d543da6bcf6c18f3b9b87d1dbb8071e04cabad1
Lucas <lucasgen08@gmail.com>
2020-08-05 02:18:42 -0700

    small things

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from . import forms
 # Create your views here.
 
 
+# see docimentation here: https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/
 class SignUp(CreateView):
     form_class = forms.UserCreateForm  # this is just setting them equal to each other, no instantiating
     success_url = reverse_lazy('login')  # on a successful sign up, they will be redirected to login page
