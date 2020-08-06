39e1b0e246c2ee3978e959c9bbe623ee735d2d2b
Lucas <lucasgen08@gmail.com>
2020-08-05 21:05:01 -0700

    small stuff

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from django.utils.text import slugify
+# from accounts.models import User
 
-# Create your models here.
+
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+# https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/#inclusion-tags
+# This is for the in_group_members check template tag
+from django import template
+register = template.Library()
+
+class City(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+        # potentially helpful link: https://stackoverflow.com/questions/43179875/when-to-use-django-get-absolute-url-method
+    def get_absolute_url(self):
+        return reverse("cities:single", kwargs={"slug": self.slug})
+    # the 'groups' refers to the app_name in the urls.py of the app and the 'single' refers to the name of the path
+    # the kwargs are passed into the urls.py in the url. check the social clone app groups/urls.py for reference
+
+    # just tells django what the default ordering is
+    class Meta:
+        ordering = ["name"]
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+from django.urls import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from groups.models import Group,GroupMember
+from . import models
 
-# Create your views here.
+
+
+
+
+# documentation can be found here: https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-display/
+
+# I think this automatically searches for template names if none are defined, but you can also define a "template_name"
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+    # doc here: https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-display/
+class ListGroups(generic.ListView):
+    model = Group
+    # good choice for template name is something like "cities_list.html"
