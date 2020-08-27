c52fa561338fe25ce0ab9a146fe7aeb4f344a63a
Lucas <lucasgen08@gmail.com>
2020-08-27 03:11:42 -0700

    adds filters to the state and city pages

--- cities/urls.py
+++ cities/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("posts/in/<slug>/",views.SingleCity.as_view(), name="single"),
     path('states/', views.ListStates.as_view(), name='list_states'), 
     path('cities/in/<slug>/', views.SingleState.as_view(), name='state_detail'),
+
+    path('create/all/cities/', views.make_all_states, name='make_all_states'),
 ]
 
 
