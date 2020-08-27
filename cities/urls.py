d68a0b84e39f68117d0c65ef2bada0a0f0bf608b
Lucas <lucasgen08@gmail.com>
2020-08-27 03:51:40 -0700

    just made all california cities!!

--- cities/urls.py
+++ cities/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('states/', views.ListStates.as_view(), name='list_states'), 
     path('cities/in/<slug>/', views.SingleState.as_view(), name='state_detail'),
 
-    path('create/all/cities/', views.make_all_states, name='make_all_states'),
+    path('create/all/states/', views.make_all_states, name='make_all_states'),
+    path('create/cities/', views.make_california_cities, name='make_california_cities'),
 ]
 
 
